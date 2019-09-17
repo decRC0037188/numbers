42c073474ff98ecbb6bd4764eec3b8cc50adc645
Declan Hitchmough <rc0037188@student.riversidecollege.ac.uk>
2019-09-17 12:29:20 +0100

    i

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,15 +15,27 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            double Pi = 3.141592653589793;
+            double c = 299792458;
             double d1 = 5e6;
-            richTextBox1.AppendText(d1.ToString() + "\n");
-            richTextBox1.AppendText(d1.ToString("N") + "\n");
-            richTextBox1.AppendText(d1.ToString("N3") + "\n");
-            richTextBox1.AppendText(d1.ToString("N0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+            richTextBox1.AppendText(Pi.ToString() + "\n";
+            richTextBox1.AppendText(Pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(Pi.Tostring("N3") + "\n");
+            richTextBox1.AppendText(Pi.Tostring("N15") + "\n");
+            richTextBox1.AppendText(Pi.ToString("0") + "\n");
+            richTextBox1.AppendText(Pi.ToString("0.0") + "\n");
+            richTextBox1.AppendText(Pi.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(Pi.toString("0.00E00") + "\n");
+
+
+
+
+
+
+
+
+
+
         }
         
             private void Form1_Load(object sender, EventArgs e)
